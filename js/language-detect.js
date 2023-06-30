7c6456635863238035545e8b112ecbf7340a5db3
Angel Alfaro <lalfaro@dragoncem.com>
2023-06-30 11:19:36 -0600

    urls

--- js/language-detect.js
+++ js/language-detect.js
@@ -12,16 +12,16 @@ $(document).ready(function(){
         
         // Add your language section URLs and corresponding language codes here
         var languageSections = {
-            "en": "http://127.0.0.1/sitio%20web/en-US/",
-            "en-US": "http://127.0.0.1/sitio%20web/en-US/",
+            "en": "https://www.dragoncem.com/v3/en-US/",
+            "en-US": "https://www.dragoncem.com/v3/en-US/",
 
-            "es": "http://127.0.0.1/sitio%20web/es-MX/",
-            "es-US": "http://127.0.0.1/sitio%20web/es-MX/",
-            "es-MX": "http://127.0.0.1/sitio%20web/es-MX/",
-            "es-419": "http://127.0.0.1/sitio%20web/es-MX/",
+            "es": "https://www.dragoncem.com/v3/es-MX/",
+            "es-US": "https://www.dragoncem.com/v3/es-MX/",
+            "es-MX": "https://www.dragoncem.com/v3/es-MX/",
+            "es-419": "https://www.dragoncem.com/v3/es-MX/",
             
-            "pt": "http://127.0.0.1/sitio%20web/pt-BR/",
-            "pt-BR": "http://127.0.0.1/sitio%20web/pt-BR/",
+            "pt": "https://www.dragoncem.com/v3/pt-BR/",
+            "pt-BR": "https://www.dragoncem.com/v3/pt-BR/",
             // Add more language sections as needed
         };
         
@@ -30,7 +30,7 @@ $(document).ready(function(){
             window.location.href = languageSections[browserLanguage];
         } else {
             // Redirect to a default language section if browser language is not supported
-            window.location.href = "http://127.0.0.1/sitio%20web/es-MX/";
+            window.location.href = "https://www.dragoncem.com/v3/es-MX/";
         }
     }
     
