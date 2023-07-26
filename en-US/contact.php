baa23c21ffbf7efecb2ccdec27ae4836c5aa0043
Angel Alfaro <129906496+lalfaro-dragoncem@users.noreply.github.com>
2023-07-26 17:19:34 -0600

    se agrega etiqueta de meta

--- en-US/contact.php
+++ en-US/contact.php
@@ -46,11 +46,28 @@
         'phone_conversion_number': '55 41 61 17 91' 
     }); 
     </script>
+
+    <!-- Meta Pixel Code -->
+    <script>
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '3148044822089415');
+      fbq('track', 'PageView');
+    </script>
+    <noscript>
+      <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=3148044822089415&ev=PageView&noscript=1"/>
+    </noscript>
+    <!-- End Meta Pixel Code -->
         
     <!-- Bootstrap , fonts & icons -->
     <link href="../css/bootstrap.min.css" rel="stylesheet" />
     <link href="../icons-pack/fontawesome-5/css/all.css" rel="stylesheet" />
-
 
     <!-- main css file -->
     <link href="../css/main.min.css" rel="stylesheet" />
